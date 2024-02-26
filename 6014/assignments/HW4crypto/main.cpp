9c2c31d669435d92523229d47509f770a9d50d12
aidenpratt <u1458057@umail.utah.edu>
2024-02-26 09:42:12 -0700

    fixes to crypto hw

--- 6014/assignments/HW4crypto/main.cpp
+++ 6014/assignments/HW4crypto/main.cpp
@@ -184,85 +184,90 @@ int main() {
     //uncomment sections below:
 
 
-//    std::vector<uint8_t> rc4Message = {'h', 'e', 'l','l','o','m','i','s','t','e','r'};
-//    std::cout << "original rc4 message: ";
-//    printBitsToStringArray(rc4Message);
-//
-//    std::vector<uint8_t> rc4Message2 = {'h', 'i','d','u','d','e'};
-//    std::cout << "original rc4 message: ";
-//    printBitsToStringArray(rc4Message);
+    std::vector<uint8_t> rc4Message = {'h', 'e', 'l','l','o','m','i','s','t','e','r'};
+    std::cout << "original rc4 message1: ";
+    printBitsToStringArray(rc4Message);
 
-//
-//    std::vector<uint8_t> rc4Key = {'g', 'o', 'o', 'd', 'k', 'e', 'y', 'n', 'a', 'm' ,'e'};
-//    RC4 rc4(rc4Key);
-//
-//    std::vector<uint8_t> encryptedRC4 = rc4Message;
-//    rc4.rc4EncrpytDecrypt(encryptedRC4);
-//    std::cout << "encrypted message: ";
-//    printBitsToStringArray(encryptedRC4);
-//
-//    rc4 = RC4(rc4Key);
-//    std::vector<uint8_t> decryptedRC4 = encryptedRC4;
-//    rc4.rc4EncrpytDecrypt(decryptedRC4);
-//    std::cout << "Decrypted message with correct key: ";
-//    printBitsToStringArray(decryptedRC4);
-//
-//    std::vector<uint8_t> rc4BadKey = {'b', 'a', 'd', 'k', 'e', 'y'};
-//    RC4 badRC4Decryption(rc4BadKey);
-//    std::vector<uint8_t> badKeyDecryptRC4 = encryptedRC4;
-//    badRC4Decryption.rc4EncrpytDecrypt(badKeyDecryptRC4);
-//    std::cout << "bad key decrypted message does not reveal plain text: ";
-//    printBitsToStringArray(badKeyDecryptRC4);
-//
-//
-//    std::vector<uint8_t> encryptedRC42 = rc4Message2;
-//    rc4.rc4EncrpytDecrypt(encryptedRC42);
-//    std::cout << "encrypted second message: ";
-//    printBitsToStringArray(encryptedRC42);
-//
-//
-//    //xor two messages using the same key to see results:
-//    //reveals some of the messages
-//    for (size_t i = 0; i < encryptedRC4.size(); ++i) {
-//        encryptedRC4[i] ^= encryptedRC42[i];
-//    }
+    std::vector<uint8_t> rc4Message2 = {'h', 'i','d','u','d','e'};
+    std::cout << "original rc4 message2: ";
+    printBitsToStringArray(rc4Message2);
+
+
+    std::vector<uint8_t> rc4Key = {'g', 'o', 'o', 'd', 'k', 'e', 'y', 'n', 'a', 'm' ,'e'};
+    RC4 rc4(rc4Key);
+
+    std::vector<uint8_t> encryptedRC4 = rc4Message;
+    rc4.rc4EncrpytDecrypt(encryptedRC4);
+    std::cout << "encrypted message1: ";
+    printBitsToStringArray(encryptedRC4);
+
+    rc4 = RC4(rc4Key);
+    std::vector<uint8_t> decryptedRC4 = encryptedRC4;
+    rc4.rc4EncrpytDecrypt(decryptedRC4);
+    std::cout << "Decrypted message1 with correct key: ";
+    printBitsToStringArray(decryptedRC4);
+
+    std::vector<uint8_t> rc4BadKey = {'b', 'a', 'd', 'k', 'e', 'y'};
+    RC4 badRC4Decryption(rc4BadKey);
+    std::vector<uint8_t> badKeyDecryptRC4 = encryptedRC4;
+    badRC4Decryption.rc4EncrpytDecrypt(badKeyDecryptRC4);
+    std::cout << "bad key decrypted message does not reveal plain text: ";
+    printBitsToStringArray(badKeyDecryptRC4);
+
+
+    std::vector<uint8_t> encryptedRC42 = rc4Message2;
+    rc4.rc4EncrpytDecrypt(encryptedRC42);
+    std::cout << "encrypted second message2: ";
+    printBitsToStringArray(encryptedRC42);
+
+
+    //xor two messages using the same key to see results:
+    //reveals some of the messages
+    for (size_t i = 0; i < encryptedRC4.size(); ++i) {
+        encryptedRC4[i] ^= encryptedRC42[i];
+    }
+    std::cout << "encrypted message after XOR the two messages: ";
+    printBitsToStringArray(encryptedRC4);
+
+
+    //SALARY SECTION!::
 
     //modifying a message using a bit flipping attack:
-//    std::vector<uint8_t> moneyMessage = {'Y', 'o', 'u','r',' ', 's','a','l','a','r','y',' ', 'i','s', ' ', '$', '1','0','0','0'};
-//    std::cout << "original money message: ";
-//    printBitsToStringArray(moneyMessage);
-//
-//    std::vector<uint8_t> moneyEncryptionRC4 = moneyMessage;
-//    rc4.rc4EncrpytDecrypt(moneyEncryptionRC4);
-//    std::cout << "encrypted money RC4 message: ";
-//    printBitsToStringArray(moneyEncryptionRC4);
-//
-//
-//    // Modifying the Cipher Text (bit-flipping attack)
-//    std::vector<uint8_t> modifiedMoneyMessage = {'9', '9', '9', '9'};
-//
-//    // XOR the original ciphertext with the XOR of original and modified plaintexts
-//    for (size_t i = 0; i < modifiedMoneyMessage.size(); i++) {
-//        moneyEncryptionRC4[i + 16] ^= (moneyMessage[i+ 16] ^ modifiedMoneyMessage[i]);
-//    }
-//
-//    std::cout << "Modified encrypted money RC4 message: ";
-//    printBitsToStringArray(moneyEncryptionRC4);
-//
-//    // Decrypt the modified ciphertext
-//    rc4 = RC4(rc4Key); // Reinitialize RC4 with the original key
-//    std::vector<uint8_t> modifiedDecryptionRC4 = moneyEncryptionRC4;
-//    rc4.rc4EncrpytDecrypt(modifiedDecryptionRC4);
-//    std::cout << "Decrypted modified money RC4 message: ";
-//    printBitsToStringArray(modifiedDecryptionRC4);
+    std::vector<uint8_t> moneyMessage = {'Y', 'o', 'u','r',' ', 's','a','l','a','r','y',' ', 'i','s', ' ', '$', '1','0','0','0'};
+    std::cout << "original money message: ";
+    printBitsToStringArray(moneyMessage);
+    rc4 = RC4(rc4Key); //restating this key before sending the money messages
+    std::vector<uint8_t> moneyEncryptionRC4 = moneyMessage;
+    rc4.rc4EncrpytDecrypt(moneyEncryptionRC4);
+    std::cout << "encrypted money RC4 message: ";
+    printBitsToStringArray(moneyEncryptionRC4);
+
+
+    // Modifying the Cipher Text (bit-flipping attack)
+    std::vector<uint8_t> modifiedMoneyMessage = {'9', '9', '9', '9'};
+
+    // XOR the original ciphertext with the XOR of original and modified plaintexts
+    for (size_t i = 0; i < modifiedMoneyMessage.size(); i++) {
+        moneyEncryptionRC4[i + 16] ^= (moneyMessage[i+ 16] ^ modifiedMoneyMessage[i]);
+    }
+
+    std::cout << "Modified encrypted money RC4 message: ";
+    printBitsToStringArray(moneyEncryptionRC4);
+
+    // Decrypt the modified ciphertext
+    rc4 = RC4(rc4Key); // Reinitialize RC4 with the original key
+    std::vector<uint8_t> modifiedDecryptionRC4 = moneyEncryptionRC4;
+    rc4.rc4EncrpytDecrypt(modifiedDecryptionRC4);
+    std::cout << "Decrypted modified money RC4 message: ";
+    printBitsToStringArray(modifiedDecryptionRC4);
 
 
 
     //-------------------------------------MY AES IMPLEMENTATION: -----------------------------------------------//
 
     //uncomment sections below:
-//
-//    //setting up the substitution tables:
+
+    //setting up the substitution tables:
 //    std::vector<SubTable> substitutionTables(8, SubTable(256));
 //    for (SubTable &table: substitutionTables) {
 //        fisherYates(table);
